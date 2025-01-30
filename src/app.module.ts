395aa3dd425c182a1e910045011d43b1c39294f7
quarerma <gabriel.oliveira.quaresma@gmail.com>
2025-01-30 12:00:08 -0300

    fix injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DataBaseService } from './database/database.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [DataBaseService],
 })
